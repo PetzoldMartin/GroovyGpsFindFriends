2284b62c82c0969c4b319865f660e603a5744420
Dikay900 <dark900@xyz.de>
2015-11-26 11:22:01 +0100

    Openstreetmap with first own marker

--- FindMEClient/app/src/main/groovy/com/example/aisma/findmeclient/MainActivity.groovy
+++ FindMEClient/app/src/main/groovy/com/example/aisma/findmeclient/MainActivity.groovy
@@ -6,8 +6,20 @@ import android.widget.Toast
 import com.arasthel.swissknife.SwissKnife
 import com.arasthel.swissknife.annotations.OnClick;
 
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import android.app.Activity;
+import android.os.Bundle
+import org.osmdroid.views.overlay.ItemizedIconOverlay
+import org.osmdroid.views.overlay.OverlayItem;
+
 public class MainActivity extends AppCompatActivity {
     def ILocator
+    def mMapView;
+    def mMapController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
         SwissKnife.restoreState(this, savedInstanceState);
         // This mus be called for automatic parsing of intent extras
         SwissKnife.loadExtras(this)
+
+        mMapView = (MapView) findViewById(R.id.mapview);
+        mMapView.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
+        mMapView.setBuiltInZoomControls(true);
+        mMapController = (MapController) mMapView.getController();
+        mMapController.setZoom(13);
+        def gPt = new GeoPoint(0,0);
+        mMapController.setCenter(gPt);
+
         ILocator=new ClientLocator(this);
 
     }
@@ -25,7 +46,27 @@ public class MainActivity extends AppCompatActivity {
     @OnClick(R.id.test)
     public void onClick() {
         Toast.makeText(this, ILocator.toString(), Toast.LENGTH_SHORT).show()
+            if (ILocator.getLocation().x == null)
+            return;
+        def gPt = new GeoPoint(ILocator.getLocation().x ,ILocator.getLocation().y);
+        mMapController.setCenter(gPt);
+        // Create an ArrayList with overlays to display objects on map
+        def overlayItemArray = new ArrayList<OverlayItem>();
+
+        // Create som init objects
+        OverlayItem centerMe = new OverlayItem("Ich bin Hier", "Zweite Zeile",
+                new GeoPoint(ILocator.getLocation().x,ILocator.getLocation().y));
+
+        // Add the init objects to the ArrayList overlayItemArray
+        overlayItemArray.add(centerMe);
+
+        // Add the Array to the IconOverlay
+        ItemizedIconOverlay<OverlayItem> itemizedIconOverlay = new ItemizedIconOverlay<OverlayItem>(this, overlayItemArray, null);
+
+        // Add the overlay to the MapView
+        mMapView.getOverlays().add(itemizedIconOverlay);
 
     }
+
 }
 
