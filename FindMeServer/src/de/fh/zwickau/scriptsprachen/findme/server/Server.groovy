01d07a47f9b22b6809ea0b62ce57573424a7c2bd
Tobias Schießl <tos111j8@fh-zwickau.de>
2015-12-02 09:34:43 +0100

    initialized server with correct IP (note: an exception has to be added to the windows firewall)

--- FindMeServer/src/de/fh/zwickau/scriptsprachen/findme/server/Server.groovy
+++ FindMeServer/src/de/fh/zwickau/scriptsprachen/findme/server/Server.groovy
@@ -5,11 +5,11 @@ import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory
 import org.glassfish.jersey.server.ResourceConfig
 
 import de.fh.zwickau.scriptsprachen.findme.server.resource.*;
- final ResourceConfig rc = new ResourceConfig();
+final ResourceConfig rc = new ResourceConfig();
 
-        rc.register(Auth.class)
-		rc.register(Mediator.class)
-final HttpServer server =GrizzlyHttpServerFactory.createHttpServer("http://localhost:8080".toURI(), rc)
+rc.register(Auth.class)
+rc.register(Mediator.class)
+final HttpServer server =GrizzlyHttpServerFactory.createHttpServer("http://141.32.20.48:8080".toURI(), rc)
 
 String email="testemail"
 String name="testname"
@@ -17,8 +17,8 @@ Auth.eMailAddresses.add(email)
 Auth.names.put(email, name)
 Auth.isLoggedIn.put(email, true)
 
- email="onlinemail"
- name="online"
+email="onlinemail"
+name="online"
 Auth.eMailAddresses.add(email)
 Auth.names.put(email, name)
 Auth.isLoggedIn.put(email, true)
