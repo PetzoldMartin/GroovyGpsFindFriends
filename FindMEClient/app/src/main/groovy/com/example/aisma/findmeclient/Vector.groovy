4be5d6809dfbe9a22882abbf30901d6333bac2f0
Tobias Schießl <tos111j8@fh-zwickau.de>
2015-12-07 14:08:33 +0100

    added toString and fromString methods

--- FindMEClient/app/src/main/groovy/com/example/aisma/findmeclient/Vector.groovy
+++ FindMEClient/app/src/main/groovy/com/example/aisma/findmeclient/Vector.groovy
@@ -1,7 +1,24 @@
 package com.example.aisma.findmeclient;
 
 class Vector {
+
     def x, y
 
-    Vector() {}
+    @Override
+    public String toString() {
+        return x + " " + y
+    }
+
+    public static Vector fromString(String s) {
+        try {
+            Vector v = new Vector()
+            String[] coords = s.split(" ")
+            v.x = Double.parseDouble(coords[0])
+            v.y = Double.parseDouble(coords[1])
+            return v
+        } catch (NumberFormatException ex) {
+            println "Error while parsing Vector from String: " + ex
+        }
+    }
+
 }
