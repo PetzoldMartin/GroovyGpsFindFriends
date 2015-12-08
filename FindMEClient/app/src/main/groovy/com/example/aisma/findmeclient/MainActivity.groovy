2237e7d2259040a9aeccd9f4c23b3ec9732a03ba
Aismael <Aismaelinc@web.de>
2015-12-09 00:22:34 +0100

    Funktional Sidebar before Friendslist

--- FindMEClient/app/src/main/groovy/com/example/aisma/findmeclient/MainActivity.groovy
+++ FindMEClient/app/src/main/groovy/com/example/aisma/findmeclient/MainActivity.groovy
@@ -1,11 +1,22 @@
 package com.example.aisma.findmeclient
 
+import android.content.res.Configuration
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 import android.view.LayoutInflater
+import android.view.MenuItem
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.FrameLayout
+import android.widget.ListView
+import android.widget.TextView
+import android.widget.Toast
 import com.arasthel.swissknife.SwissKnife
 import com.arasthel.swissknife.annotations.OnClick
+import com.arasthel.swissknife.annotations.OnItemClick
 import org.osmdroid.views.MapView
 import android.os.Bundle
 
@@ -14,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
     def openStreetMap;
     RESTRequests restRequests;
     RESTServer restServer;
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private DrawerLayout mDrawerLayout;
+    private String mActivityTitle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +47,35 @@ public class MainActivity extends AppCompatActivity {
         openStreetMap = new OpenStreetMap(this, (MapView) findViewById(R.id.mapview), ILocator)
         restRequests = new RESTRequests()
         restServer = new RESTServer()
-
+        //def toolbar = (Toolbar) findViewById(R.id.toolbar)
+        //setSupportActionBar(toolbar)
         getSupportActionBar().show();
+
+
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        mDrawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);
+        mActivityTitle = getTitle().toString();
+        addDrawerItems()
+        setupDrawer()
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setHomeButtonEnabled(true);
+    }
+
+    @OnItemClick(R.id.navList)
+    public void changeInlay(int position,AdapterView<?> parent,View view){
+        String item = ((TextView)view).getText().toString();
+        Toast.makeText(baseContext, "Time for an upgrade!"+item, Toast.LENGTH_SHORT).show();
+        switch (item){
+            case "Map":
+                this.setLayoutInlay(R.layout.activity_main);
+                break
+            case "Friendslist":
+                this.setLayoutInlay(R.layout.friends_list);
+                break
+        }
+
+
+
     }
 
     @OnClick(R.id.jetty)
@@ -56,8 +99,62 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setLayoutInlay(id){
+        ((FrameLayout) findViewById(R.id.content_frame)).removeAllViews()
+
+
         LayoutInflater inflater = getLayoutInflater();
         View v = inflater.inflate(id, null);
         ((FrameLayout)findViewById(R.id.content_frame)).addView(v);
     }
+    private void addDrawerItems() {
+        String[] InlayArray = ["Map","Friendslist"];
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, InlayArray);
+        mDrawerList.setAdapter(mAdapter);
+    }
+
+    private void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle("Navigation!");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mActivityTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        mDrawerToggle.setDrawerIndicatorEnabled(true);
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        // Activate the navigation drawer toggle
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
